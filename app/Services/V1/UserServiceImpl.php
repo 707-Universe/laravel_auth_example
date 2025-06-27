46d25320c260c10443e8b0f64488cebbbc799006
Youki Takemoto <707.universe@gmail.com>
2025-06-27 21:47:28 +0900

    commit

--- app/Services/V1/UserServiceImpl.php
+++ app/Services/V1/UserServiceImpl.php
@@ -10,6 +10,7 @@ class UserServiceImpl implements UserService
 {
     public function createFromRequest(CreateRequest $request): User
     {
+        // Todo: 入力と一致するメールアドレスが未アクティベートの場合は削除する
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
